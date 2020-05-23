68e06428b03cffcc642e45309f3807fa371f2578
gcaramatti <biel.caramatti180@gmail.com>
2020-05-23 13:24:14 -0300

    Crud exercicio

---
+++ LoginPage.php
@@ -0,0 +1,14 @@
+<?php
+$link = mysqli_connect($host="127.0.0.1", $user="root", $password="", $database="bd_php");
+
+if (!$link) {
+    echo "Error: Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+} 
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+?>
